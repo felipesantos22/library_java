59b933553786deef91f61cd1af416b939b3c9d07
unknown <felipevs2018@gmail.com>
2023-07-16 02:43:10 -0300

    refatoring address and human

--- library/src/main/java/com/example/library/entity/Address.java
+++ library/src/main/java/com/example/library/entity/Address.java
@@ -1,8 +1,6 @@
 package com.example.library.entity;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -17,25 +15,21 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonIdentityInfo(
-    generator = ObjectIdGenerators.PropertyGenerator.class,
-    property = "id"
-)
 public class Address {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(name = "id")
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
 
-  @Column
-  private String city;
+    @Column
+    private String city;
 
-  @Column
-  private String street;
+    @Column
+    private String street;
 
-  @OneToOne(mappedBy = "address")
-  @JsonManagedReference
-  private Human human;
+    @OneToOne(mappedBy = "address")
+    @JsonIgnoreProperties("address")
+    private Human human;
 
 }
--- library/src/main/java/com/example/library/entity/Human.java
+++ library/src/main/java/com/example/library/entity/Human.java
@@ -1,8 +1,6 @@
 package com.example.library.entity;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -19,22 +17,18 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonIdentityInfo(
-    generator = ObjectIdGenerators.PropertyGenerator.class,
-    property = "id"
-)
 public class Human {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-  @Column
-  private String username;
+    @Column
+    private String username;
 
-  @OneToOne(cascade = CascadeType.ALL)
-  @JoinColumn(name = "address_id", referencedColumnName = "id")
-  @JsonManagedReference
-  private Address address;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    @JsonIgnoreProperties("human")
+    private Address address;
 
 }
